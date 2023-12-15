5e845e5e2701ff931b830fbe6d6ab5b81289a36c
dsl2022 <2015rpro@gmail.com>
2023-12-15 11:50:33 -0500

    setup github action

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Node runtime as a parent image
+FROM node:16
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install any needed packages
+RUN npm install
+
+# Bundle app source inside Docker image
+COPY . .
+
+# Make port available to the world outside this container
+EXPOSE 8080
+
+# Run app when the container launches
+CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const cors = require('cors');
-
+app.use(express.json()); 
 // Use CORS with default settings (allow requests from any origin)
 app.use(cors());
 
@@ -19,26 +19,49 @@ function setSSEHeaders(req, res, next) {
     res.flushHeaders(); // flush the headers to establish SSE with the client
     next();
 }
-
+let clients = [];
 // Route for chat endpoint
 app.get('/chat', setSSEHeaders, (req, res) => {
     // Function to send a message
-    const sendMessage = (message) => {
-        res.write(`data: ${JSON.stringify(message)}\n\n`);
-    };
 
-    // Example: Sending a welcome message
-    sendMessage({ user: 'Server', text: `To create a chat interface similar to ChatGPT that renders messages word-by-word, you can modify your client-side code (in your React application) to handle this type of rendering. This requires a bit of additional logic to progressively display the message.
+    clients.push(res);
+    // const sendMessage = (message) => {
+    //     res.write(`data: ${JSON.stringify(message)}\n\n`);
+    // };
 
-    Here’s a way to implement this:` });
+    // // Example: Sending a welcome message
+    // sendMessage({ user: 'Server', text: `To create a chat interface similar to ChatGPT that renders messages word-by-word, you can modify your client-side code (in your React application) to handle this type of rendering. This requires a bit of additional logic to progressively display the message.
 
-    // You can integrate your chat logic here, e.g., listen to a chat service or database and send messages.
+    // Here’s a way to implement this:` });
+
+    // // You can integrate your chat logic here, e.g., listen to a chat service or database and send messages.
 
     // Handle client disconnect
     req.on('close', () => {
         console.log('Client disconnected');
         // Clean up resources or stop listening to chat service.
     });
+});
+
+// Function to send a message to all connected clients
+function sendMessageToClients(message) {
+    clients.forEach(client =>
+        client.write(`data: ${JSON.stringify(message)}\n\n`)
+    );
+}
+
+// Endpoint to receive messages from the client
+app.post('/send-message', (req, res) => {
+    const userMessage = req.body.message;
+    
+    // Here, you would typically include your AI chat logic or processing
+    // For demonstration, let's just echo the message back
+    const aiResponse = `Echo: ${userMessage}`;
+
+    // Send the AI response to all connected clients
+    sendMessageToClients({ user: 'AI', text: aiResponse });
+
+    res.status(200).json({ message: "Message received" });
 });
 
 app.listen(PORT, () => {
