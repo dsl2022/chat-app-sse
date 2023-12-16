5e65fad38e231bff6f285bc51eb8c953dc7db987
dsl2022 <2015rpro@gmail.com>
2023-12-16 12:56:31 -0500

    updated terraform with igw and route table

--- .terraform/ecs.tf
+++ .terraform/ecs.tf
@@ -9,6 +9,7 @@ resource "aws_ecs_task_definition" "app_task" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn       = aws_iam_role.ecs_execution_role.arn
   container_definitions = jsonencode([
     {
       name  = "chat-app",
