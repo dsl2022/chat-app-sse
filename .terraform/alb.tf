f70eb471f77c63af3b71516986a00fe498d2e4a0
dsl2022 <2015rpro@gmail.com>
2023-12-16 12:26:18 -0500

    updated terraform with igw and route table

--- .terraform/alb.tf
+++ .terraform/alb.tf
@@ -20,7 +20,7 @@ resource "aws_lb_target_group" "app_tg" {
     unhealthy_threshold = 2
     timeout             = 5
     interval            = 30
-    path                = "/"
+    path                = "/health"
     protocol            = "HTTP"
     matcher             = "200"
   }
