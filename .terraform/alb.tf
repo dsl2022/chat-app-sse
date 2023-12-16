4a43bf3f02dc174ae061f501462f3f2f98f63bb6
dsl2022 <2015rpro@gmail.com>
2023-12-15 21:12:38 -0500

    updated terraform with igw and route table

--- .terraform/alb.tf
+++ .terraform/alb.tf
@@ -12,7 +12,8 @@ resource "aws_lb_target_group" "app_tg" {
   name     = "chat-app-target-group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.app_vpc.id
+  target_type = "ip"
+  vpc_id   = aws_vpc.chat_app_vpc.id
 
   health_check {
     healthy_threshold   = 2
