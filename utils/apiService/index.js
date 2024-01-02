470b64a8acf782b2e2e7dc48a932e7b722dfccd5
dsl2022 <2015rpro@gmail.com>
2024-01-01 19:15:32 -0500

    added log for health check

--- utils/apiService/index.js
+++ utils/apiService/index.js
@@ -4,6 +4,8 @@ dotenv.config()
 const BASE_MOCK_API_URL=process.env.BASE_MOCK_API_URL
 const NODE_ENV = process.env.NODE_ENV
 const BASE_URL = NODE_ENV==="dev"? "http://localhost:4000":BASE_MOCK_API_URL
+console.log("BASE_MOCK_API_URL",BASE_MOCK_API_URL)
+console.log("NODE_ENV",NODE_ENV)
 const makeRequest = async (endpoint, method) => {
   try {
     let response;
