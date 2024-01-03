e9f4eb174076d3f17a04f0cb29ed2fc5794de506
dsl2022 <2015rpro@gmail.com>
2024-01-03 00:48:09 -0500

    updated

--- utils/apiService/index.js
+++ utils/apiService/index.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config()
-const {BASE_MOCK_API_URL}=process.env.BASE_MOCK_API_URL
-const {NODE_ENV} = process.env.NODE_ENV
+const {BASE_MOCK_API_URL}= JSON.parse(process.env.BASE_MOCK_API_URL)
+const {NODE_ENV} = JSON.parse(process.env.NODE_ENV)
 const BASE_URL = NODE_ENV==="dev"? "http://localhost:4000":BASE_MOCK_API_URL
 console.log("BASE_MOCK_API_URL",BASE_MOCK_API_URL)
 console.log("NODE_ENV",NODE_ENV)
